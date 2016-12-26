144f4de658f8188df440e4de893c5405301eeda8
Lehel Iszlai <iszlai.lehel@gmail.com>
2016-12-27 00:19:32 +0200

    Spray

--- server/src/main/scala/LinkrService.scala
+++ server/src/main/scala/LinkrService.scala
@@ -1,10 +1,10 @@
-import akka.actor.Actor
-import spray.routing._
-import spray.http._
-import MediaTypes._
 import MyJsonProtocol._
+import akka.actor.Actor
+import spray.http._
 import spray.httpx.SprayJsonSupport
-import spray.json._
+import spray.routing._
+import spray.http.HttpHeaders._
+import spray.http.HttpMethods._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -22,34 +22,49 @@ class LinkrService extends Actor with MyService {
 
 
 // this trait defines our service behavior independently from the service actor
-trait MyService extends HttpService with SprayJsonSupport {
+trait MyService extends HttpService with SprayJsonSupport with CORSSupport {
 
-  override val myRoute =
-    path("echo") {
-      get {
-        complete {
-          Person("Name", 12)
-        }
-
+   val myRoute = respondWithCORS("*"){
+  path("echo") {
+    get {
+      complete {
+        Person("Name", 12)
       }
-    } ~ path("login") {
-      post {
-        entity(as[User]) {
-          user =>
-            if (user.name == "lehel") {
-              setCookie(HttpCookie("##lehel##", "bar")) {
-                complete {
-                  print(user)
-                  "ok"
-                }
-            }
-            } else{
-              complete {
-                print(user)
-                "nok"
-              }
-            }
-        }
+
+    }
+  } ~ path("login") {
+    post {
+      entity(as[User]) { user =>
+        authenticator(user).map {
+          credential =>
+            setCookie(HttpCookie("###Lehel##" + credential.name, "Bar",domain=Some("localhost")))
+            complete(StatusCodes.OK)
+        }.getOrElse(complete(StatusCodes.Unauthorized))
       }
     }
+  }
+  }
+
+  def authenticator(user: User): Option[User] = {
+    if (user.name == "lehel") Some(user) else None
+  }
+
 }
+
+trait CORSSupport extends Directives {
+  private val CORSHeaders = List(
+    `Access-Control-Allow-Methods`(GET, POST, PUT, DELETE, OPTIONS),
+    `Access-Control-Allow-Headers`("Origin, X-Requested-With, Content-Type, Accept, Accept-Encoding, Accept-Language, Host, Referer, User-Agent"),
+    `Access-Control-Allow-Credentials`(true)
+  )
+
+  def respondWithCORS(origin: String)(routes: => Route) = {
+    val originHeader = `Access-Control-Allow-Origin`(AllOrigins)
+    respondWithHeaders(originHeader :: CORSHeaders) {
+      routes ~ options {
+        complete(StatusCodes.OK)
+      }
+    }
+  }
+}
+
